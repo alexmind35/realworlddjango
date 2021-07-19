1125fd34f55e6ca61b1bc6060c4e2b0850b3e976
Alexander Mikhaylov <alexmind@yandex.ru>
2021-07-19 12:26:35 +0300

    Initial commit

---
+++ events/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название', default='')
+    description = models.TextField(verbose_name='Название', default='')
+    date_start = models.DateTimeField(verbose_name='Дата начала')
+    participants_number = models.PositiveSmallIntegerField(verbose_name='Количество участников')
+    is_private = models.BooleanField(verbose_name='Частное', default=False)
+
+    class Meta:
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=90, verbose_name='Категория', default="")
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
